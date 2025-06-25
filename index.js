792dc41ecda472d99a7fa179fc10f8d5ea137334
Frutos Francisco <francisco_frutos@outlook.com>
2025-06-25 15:03:05 -0300

    puppeter chrome fix2

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function scrapeSteam(pageNum = 1) {
         browser = await puppeteer.launch({
             headless: "new",
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            executablePath // Ensure Puppeteer uses the installed Chromium
+        
         });
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36');
